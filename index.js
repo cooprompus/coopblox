7cb8241f18334944b8fafbf1a80b9d575fe2612c
coop <cooprompus@gmail.com>
2019-02-14 17:58:09 -0500

    split out unfriending and listening for friend requests

--- index.js
+++ index.js
@@ -1,69 +1,94 @@
 const fs = require('fs');
 const rbx = require("noblox.js")
 
-let friends 
-let permanentFriends
-let vips
+const robloxId = 0
+const cookie = "_|WARNING:-DO-NOT-SHARE-THIS.--Sharing-this-will-allow-someone-to-log-in-as-you-and-to-steal-your-ROBUX-and-items.|_"
+
+const vipFile = "vip.txt"
+const friendFile = "friends.txt"
+const permFile = "perm.txt"
+const banFile = "ban.txt"
+
+if(process.argv[2] === "unfriend"){
+	startApp().then(() => removeFriends()).then(() => process.exit(0)).catch(console.error)
+}else{
+	startApp().then(() => acceptFriends()).catch(console.error)
+}
 
 async function startApp () {
-	await rbx.cookieLogin("_|WARNING:-DO-NOT-SHARE-THIS.--Sharing-this-will-allow-someone-to-log-in-as-you-and-to-steal-your-ROBUX-and-items.|")
+	await rbx.cookieLogin(cookie)
 }
 
 function writeFriends(friends){
-	fs.writeFile("./friends.txt", friends, (err) => {
-		if(err) {
-			return console.log(err);
-		}
+	const friendStream = fs.createWriteStream(friendFile, {'flags': 'a'});
+	friendStream.on('error', (err) => {
+		console.log('problem writing friends file')
+		console.log(err)
+	});
+	friendStream.on('finish', () => {
 		console.log("The file was saved!");
-	}); 
+	});
+	friendStream.write(',')
+	friends.forEach((friend) => friendStream.write(friend + ',\n'));
+	friendStream.end('')
 	return friends
 }
 
-function loadPermanentFriends(){
-	permanentFriends = fs.readFileSync("./perm.txt", "utf8").split(",").map((item) => parseInt(item.trim()));
-	console.log("perm:")
-	console.log(permanentFriends)
+function getFriendsList(){
+	return fs.readFileSync(friendFile, "utf8").split(",").map((item) => parseInt(item.trim()));
+}
+
+function getPermanentFriendsList(){
+	return fs.readFileSync(permFile, "utf8").split(",").map((item) => parseInt(item.trim()));
 }
 
 function loadVips(){
-	vips = fs.readFileSync("./vip.txt", "utf8").split(",").map((item) => parseInt(item.trim()));
-	console.log("vip:")
-	console.log(vips)
-}
-
-loadPermanentFriends()
-loadVips()
-startApp()
-	.then(() => {
-		return rbx.getFriends(454258140, 'AllFriends')
-	})
-	.then((result) => {
-		console.log("got friends!")
-		friends = result.friends.map(friend => friend.user.id)
-		return friends
-	})
-	.then(writeFriends)
-	.then((friends) => {
-		friends.forEach((friend) => {
-			if(vips.includes(friend)){
-				console.log(`not removing vip friend: ${friend}`)
-			}else{
-				console.log(`removing friend: ${friend}`)
-				rbx.removeFriend(friend)
-			}
+	return new Promise((resolve, reject) => {
+		fs.readFile(vipFile, "utf8", (err, buffer) => {
+			if(err) reject(err)
+			console.log(buffer)
+			resolve(buffer.split(",").map((item) => parseInt(item.trim())));
 		})
 	})
-	.then(() => {
-		const onfr = rbx.onFriendRequest();
-		onfr.on('data', (friendrequest) => {
-			console.log(friendrequest);
-			if(permanentFriends.includes(friendrequest) || friends.includes(friendrequest)){
-				console.log(`accepting friend request: ${friendrequest}`)
-				rbx.acceptFriendRequest(friendrequest)
-			}
-		});
-		onfr.on('error', (err) =>  {
-			console.error(err.stack);
-		});
+}
 
-	})
+function acceptFriends(){
+	const onfr = rbx.onFriendRequest();
+	onfr.on('data', (friendrequest) => {
+		console.log(friendrequest);
+		if(getPermanentFriendsList().includes(friendrequest) || getFriendsList().includes(friendrequest)){
+			console.log(`accepting friend request: ${friendrequest}`)
+			rbx.acceptFriendRequest(friendrequest)
+		}else{
+			console.log(`not accepting friend request: ${friendrequest}`)
+		}
+	});
+	onfr.on('error', (err) =>  {
+		console.error(err.stack);
+	});
+}
+
+async function getFriends(){
+	let result = await rbx.getFriends(robloxId, 'AllFriends')
+	return result.friends.map(friend => friend.user.id)
+}
+
+function removeFriends(){
+	return getFriends()
+		.then(writeFriends)
+		.then(removeNonVipFriends)
+}
+
+function removeNonVipFriends(friends){
+	return loadVips()
+		.then((vips) => {
+			friends.forEach((friend) => {
+				if(vips.includes(friend)){
+					console.log(`not removing vip friend: ${friend}`)
+				}else{
+					console.log(`removing friend: ${friend}`)
+					rbx.removeFriend(friend)
+				}
+			})
+		})
+}
